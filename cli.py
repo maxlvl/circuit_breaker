65e0280ae0c322577a6fcb2f53182209d49858cb
mx <max@maxvl.com>
2022-12-21 23:04:46 -0500

    WIPith pytest.assertRaises(Exception):

--- cli.py
+++ cli.py
@@ -2,20 +2,30 @@ from time import sleep
 from circuit_breaker.breaker import CircuitBreaker
 import requests
 
-cb = CircuitBreaker(max_failures=3, reset_timeout=1)
+cb = CircuitBreaker(max_failures=3, reset_timeout=3)
 
 
 @cb
-def ping_site():
-    r = requests.get("http://thisbakldaskflwsdj.com")
+def ping_site(site):
+    try:
+        if not site:
+            r = requests.get("http://thisbakldaskflwsdj.com")
+        else:
+            r = requests.get(site)
+            print("SUCCESFUL QUERY")
+    except Exception:
+        raise
+
+
+def repeat_ping(site=None):
+    try:
+        ping_site(site)
+    except Exception:
+        sleep(4)
+        site = "http://www.google.com"
+        print(f"sleeping 4 secs, site is {site}")
+        ping_site(site)
 
 
 if __name__ == "__main__":
-    try:
-        for i in range(1000):
-            ping_site()
-    except Exception:
-        print("SLEEPING 2 SECS")
-        sleep(2)
-        for i in range(100):
-            ping_site()
+    repeat_ping()

b63d8f14afdd3826881cbdacbb6b3208f750c3b6
mx <max@maxvl.com>
2022-12-21 23:50:28 -0500

    Test setup and init tests

--- tests/test_breaker.py
+++ tests/test_breaker.py
@@ -1,3 +1,13 @@
+from circuit_breaker.breaker import CircuitBreaker
+import pytest
+from time import sleep
+
+from circuit_breaker.error import CircuitBreakerError
+
+
 class TestBreaker:
-    def test_breaker__ok(self):
-        assert 1 == 1
+    def test_initial_state(self):
+        cb = CircuitBreaker(max_failures=3, reset_timeout=1)
+        assert cb.tripped is False
+        assert cb.current_failures == 0
+
